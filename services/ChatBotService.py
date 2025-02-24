140954a6359c69a53e69cb4a4b677d09ea2d9c1c
dimitrij-jovanovski <dimitrij.jovanovski.13@gmail.com>
2025-02-24 15:56:30 +0100

    Update ChatBotService.py

--- services/ChatBotService.py
+++ services/ChatBotService.py
@@ -1,1 +1,20 @@
-#############################################################
+import openai
+
+client = openai.OpenAI(api_key="my_openai_secret_key") #key is private, use your own
+
+class ChatBotService:
+    @staticmethod
+    def AskTheChatBot(prompt):
+
+        IntroString = "giving the bot character" #change
+       
+        print(prompt)
+
+        odgovor = IntroString + prompt
+
+        response = client.chat.completions.create(
+             model="gpt-4o-mini",
+             messages=[{"role": "user", "content": odgovor}]
+         ).model_dump()
+
+        return response['choices'][0]['message']['content']
